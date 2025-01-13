f52c26a719f6d4cd011c1f76a67a4ad044221081
Valentin Bouin <vbouin@lefigaro.fr>
2025-01-13 13:35:06 +0100

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import CalendarTable from './CalendarTable';
 import AddCulture from './AddCulture';
-import PotagerGrid from './PotagerGrid';
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<CalendarTable />} />
         <Route path="/add-culture" element={<AddCulture />} />
-        <Route path="/organisation" element={<PotagerGrid />} />
       </Routes>
     </Router>
   );
